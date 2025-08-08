7a1b79d197b88587ee79013ffd35ded57f31b3cd
anirudh <anirudhkhanna20@gmail.com>
2025-08-08 18:16:45 +0200

    Html code implementation

--- web.py
+++ web.py
@@ -3,6 +3,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"] + "\n"
     todos.append(todo)
@@ -12,7 +14,11 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app will increase your productivity.")
+st.write("This app will increase your <b>productivity</b>.",
+         unsafe_allow_html=True)
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
 
 
 for index, todo in enumerate(todos):
@@ -23,5 +29,3 @@ for index, todo in enumerate(todos):
         del st.session_state[todo]
         st.rerun()
 
-st.text_input(label="", placeholder="Add new todo...",
-              on_change=add_todo, key="new_todo")
